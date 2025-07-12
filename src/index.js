9144ff559d8ccb5206020c80f294df3b02319dec
OLiviER <olivier.bruch@gmail.com>
2025-07-12 13:06:47 +0100

    export

---
+++ src/index.js
@@ -0,0 +1,75 @@
+import Accordion from './components/Accordion.vue'
+import AttachmentList from './components/AttachmentList.vue'
+import Avatar from './components/Avatar.vue'
+import AvatarGroup from './components/AvatarGroup.vue'
+import AvatarInput from './components/AvatarInput.vue'
+import Badge from './components/Badge.vue'
+import Button from './components/Button.vue'
+import Card from './components/Card.vue'
+import Checkbox from './components/Checkbox.vue'
+import ClickableTag from './components/ClickableTag.vue'
+import CurrencyInput from './components/CurrencyInput.vue'
+import DatePicker from './components/DatePicker.vue'
+import DragAndDropFiles from './components/DragAndDropFiles.vue'
+import DropdownOptions from './components/DropdownOptions.vue'
+import Dropzone from './components/Dropzone.vue'
+import FileButton from './components/FileButton.vue'
+import FlashMessage from './components/FlashMessage.vue'
+import IconButton from './components/IconButton.vue'
+import Loader from './components/Loader.vue'
+import MediaCarousel from './components/MediaCarousel.vue'
+import MediaInput from './components/MediaInput.vue'
+import NumberInput from './components/NumberInput.vue'
+import Paragraph from './components/Paragraph.vue'
+import Radio from './components/Radio.vue'
+import RichTextEditor from './components/RichTextEditor.vue'
+import Select from './components/Select.vue'
+import Stepper from './components/Stepper.vue'
+import StopPropagation from './components/StopPropagation.vue'
+import Tabs from './components/Tabs.vue'
+import Tag from './components/Tag.vue'
+import Textarea from './components/Textarea.vue'
+import Textbox from './components/Textbox.vue'
+import Toggle from './components/Toggle.vue'
+import Tooltip from './components/Tooltip.vue'
+import { useFiles } from './composables/useFiles'
+import { useIsHandheld } from './composables/useIsHandheld'
+
+export {
+    Accordion,
+    AttachmentList,
+    Avatar,
+    AvatarGroup,
+    AvatarInput,
+    Badge,
+    Button,
+    Card,
+    Checkbox,
+    ClickableTag,
+    CurrencyInput,
+    DatePicker,
+    DragAndDropFiles,
+    DropdownOptions,
+    Dropzone,
+    FileButton,
+    FlashMessage,
+    IconButton,
+    Loader,
+    MediaCarousel,
+    MediaInput,
+    NumberInput,
+    Paragraph,
+    Radio,
+    RichTextEditor,
+    Select,
+    Stepper,
+    StopPropagation,
+    Tabs,
+    Tag,
+    Textarea,
+    Textbox,
+    Toggle,
+    Tooltip,
+    useFiles,
+    useIsHandheld
+}
--- vite.config.js
+++ vite.config.js
@@ -3,20 +3,20 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [vue()],
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, 'src/index.ts'),
-      name: 'UiLibrary',
-      fileName: (format) => `ui-library.${format}.js`,
-    },
-    rollupOptions: {
-      external: ['vue'],
-      output: {
-        globals: {
-          vue: 'Vue',
-        },
-      },
-    },
-  },
+	plugins: [vue()],
+	build: {
+		lib: {
+			entry: path.resolve(__dirname, 'src/index.js'),
+			name: 'UiLibrary',
+			fileName: (format) => `ui-library.${format}.js`,
+		},
+		rollupOptions: {
+			external: ['vue'],
+			output: {
+				globals: {
+					vue: 'Vue',
+				},
+			},
+		},
+	},
 })
